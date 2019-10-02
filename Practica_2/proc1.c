36729170d637aaef247b4d078182d74e2183f7da
raulacava <raulacava@gmail.com>
2019-10-02 06:41:53 -0500

    Proc 1 & 2new text only

--- Practica_2/proc1.c
+++ Practica_2/proc1.c
@@ -7,18 +7,26 @@
 #define ESC 27
 
 int main() {
+	int n;
 	char key;
 	struct text_info initial_info;
+	int xo, yo;
 
 	gettextinfo(&initial_info);
+	xo = 0; yo = 0;
+//	xo = initial_info.screenwidth /2; yo = 0;
+//	xo = 0; yo = initial_info.screenheight/2;
+//	xo = initial_info.screenwidth /2; yo = initial_info.screenheight/2;
 
-	clrscr();
+//	clrscr();
+	gotoxy(xo + 1, n = yo + 1);
 	while (key != ESC) {
 		key = getch();
 		putch(key);
-		if (wherex() >= initial_info.screenwidth)
-			if (wherey() >= initial_info.screenheight)
-				gotoxy(1, 1);
+		if (wherex() >= xo + initial_info.screenwidth/2)
+			if (wherey() >= yo + initial_info.screenheight/2)
+				gotoxy(xo + 1, n = yo + 1);
+			else gotoxy(xo + 1, ++n);
 	}
 	return 0;
 }
